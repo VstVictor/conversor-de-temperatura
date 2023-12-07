9306848c809295c33b4a61d9e6d9c104ab3e0761
VstVictor <torresvictor.silva@gmail.com>
2023-12-07 16:06:11 -0300

    commit do vst

--- conversor.py
+++ conversor.py
@@ -2,17 +2,25 @@
 
 # apresentação
 
-print('\n\t\t\t -- Conversor de Temperatura --\n')
+while True:
+    print('\n\t\t\t -- Conversor de Temperatura --\n')
 
-opcao = int(input('Escolha a opção:\n1. Celsius para Fahrenheit\n2. Fahrenheit para Celsius\nOpção: '))
+    opcao = int(input('Escolha a opção:\n1. Celsius para Fahrenheit\n2. Fahrenheit para Celsius\n3. Sair\nOpção: '))
 
-if opcao == 1:
-    cel = float(input('Digite a temperatura em Celsius: '))
-    fah = 9 * cel / 5 + 32
-    print(f'A temperatura em Fahrenheit é: {fah}')
-elif opcao == 2:
-    fah = float(input('Digite a temperatura em Fahrenheit: '))
-    cel = (fah - 32) / 9 * 5
-    print(f'A temperatura em Celsius é: {cel}')
-else:
-    print('Opção inválida. Por favor, escolha 1 ou 2.')
+    if opcao == 1:
+        cel = float(input('Digite a temperatura em Celsius: '))
+        fah = 9 * cel / 5 + 32
+        print(f'A temperatura em Fahrenheit é: {fah}')
+    elif opcao == 2:
+        fah = float(input('Digite a temperatura em Fahrenheit: '))
+        cel = (fah - 32) / 9 * 5
+        print(f'A temperatura em Celsius é: {cel}')
+    elif opcao == 3:
+        print('Até depois!')
+        break
+    else:
+        print('Opção inválida. Por favor, escolha 1, 2 ou 3.')
+
+    resposta = input('Deseja realizar outra conversão? (s/n): ').lower()
+    if resposta != 's':
+        break
